fbc7bab20cc0fcec035dede30db53ccd87d58067
teenzor <l44995577@gmail.com>
2026-04-15 05:12:54 -0300

    Add Dark Theme

--- puroAnime/scripts.js
+++ puroAnime/scripts.js
@@ -1,4 +1,14 @@
 const btntop = document.querySelector(".btntop");
+const icontheme = document.querySelector(".icon-theme");
+const body = document.body
+const headertheme = document.querySelector(".header-theme");
+const footertheme = document.querySelector(".footer-theme")
+
+icontheme.addEventListener("click", () => {
+    body.classList.toggle("ativo")
+    headertheme.classList.toggle("ativo")
+    footertheme.classList.toggle("ativo")
+})
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < 100) {
