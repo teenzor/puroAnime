d015757fc80d1feea7e82db4e97e6051e2ad299e
teenzor <l44995577@gmail.com>
2026-05-12 02:39:44 -0300

    att sintaxe js

--- puroAnime/scripts.js
+++ puroAnime/scripts.js
@@ -1,18 +1,18 @@
-const btntop = document.querySelector(".btntop");
-
-// DARK-THEME (SINTAXE)
+// THEME (SINTAXE)
 document.querySelector(".icon-theme").addEventListener("click", () => {
     document.body.classList.toggle("ativo");
 });
 
 // BTN-TOP (SINTAXE)
-window.addEventListener("scroll", () => {
-    btntop.hidden = scrollY < 300;
-});
-
-btntop.addEventListener("click", () => {
+const btn = document.querySelector(".btntop");
+btn.addEventListener("click", () => {
     scrollTo({
         top: 0,
         behavior: "smooth",
     });
+});
+
+window.addEventListener("scroll", () => {
+    btn.hidden = scrollY < 100;
+    return (btn.style.opacity = "1")
 });
